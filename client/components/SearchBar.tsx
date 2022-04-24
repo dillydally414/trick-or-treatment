310d73d3b81e9dc64337c182997f2c258add9d17
Shashank Jarmale <shashankjarmale1@gmail.com>
2022-04-24 01:01:30 -0400

    Couple of conflicts resolved

--- client/components/SearchBar.tsx
+++ client/components/SearchBar.tsx
@@ -1,22 +1,65 @@
 import React, { useState } from 'react'
+import styled from 'styled-components';
+import Color from '../styles/colors';
+import { Search } from '@mui/icons-material';
+import Font from '../styles/fonts';
+
+const SearchContainer = styled.form`
+  background-color: ${Color.LIGHT_GRAY};
+  border-radius: 0.5rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1rem;
+  width: 100%;
+`;
+
+const TextInput = styled.input`
+  background-color: transparent;
+  border-color: transparent;
+  color: ${Color.BLACK};
+  font-family: ${Font.SECONDARY};
+  font-size: 1.5rem;
+  width: 100%;
+  :focus-visible {
+    outline: none;
+  }
+  ::placeholder {
+    color: ${Color.DARK_GRAY}
+  }
+`;
+
+const SubmitButton = styled.button`
+  align-items: center;
+  background-color: transparent;
+  border-color: transparent;
+  cursor: pointer;
+  display: flex;
+`;
+
+const SearchIcon = styled(Search)`
+  color: ${Color.DARK_GRAY};
+`;
 
 interface SearchBarProps {
-    placeholder: string;
+  placeholder: string;
 }
 
 export default function SearchBar({
-    placeholder
-  }: SearchBarProps) {
-    return (
-        <form action="/" method="get">
-            <input
-                type="text"
-                id="header-search"
-                placeholder={placeholder}
-                name="s" 
-            />
-            <button type="submit">Search</button>
-        </form>
-    )
-  }
-  
+  placeholder
+}: SearchBarProps) {
+  return (
+    <SearchContainer action="/" method="get">
+      <TextInput
+        type="text"
+        id="header-search"
+        placeholder={placeholder}
+        name="s"
+      />
+      <SubmitButton type="submit">
+        <SearchIcon />
+      </SubmitButton>
+    </SearchContainer>
+  )
+}
